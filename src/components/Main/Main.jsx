2fe2e0514ccfd4a38d980c1a40620f12605bef65
KrUmang <144719853+KrUmang@users.noreply.github.com>
2025-02-12 19:00:46 +0530

    Update Main.jsx

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -26,7 +26,7 @@ const Main = () => {
             <>
               <div className="greet">
                 <p>
-                  <span>Hello, Gemini User!</span>
+                  <span>Hello, User!</span>
                 </p>
                 <p>How can I help you today?</p>
               </div>
